7f9b360226feb50842777a3e85c312ae7e36a827
Lakshan Fernando <162671765+lakshanFernando2003@users.noreply.github.com>
2024-11-26 15:02:55 +0530

    getting started from scratch

---
+++ src/main/java/CLI/System_configuration.java
@@ -0,0 +1,51 @@
+package CLI;
+
+public class System_configuration {
+    private int Total_Tickets; // Total Number Of Tickets
+    private int Ticket_Release_Rate; // Ticket Releasing Rate
+    private int Customer_Retrieval_Rate; // Customer Retrieving Rate
+    private int Max_Ticket_Capacity; // maximum capacity of Tickets
+
+        //non-parameterized constructor
+    public System_configuration(){
+
+    }
+
+    // Getters and Setters
+    public int getTotal_Tickets() {
+        return Total_Tickets;
+    }
+    public void setTotal_Tickets(int total_Tickets) {
+        Total_Tickets = total_Tickets;
+    }
+    public int getTicket_Release_Rate(){
+        return Ticket_Release_Rate;
+    }
+    public void setTicket_Release_Rate(int ticket_Release_Rate){
+        Ticket_Release_Rate = ticket_Release_Rate;
+    }
+    public int getCustomer_Retrieval_Rate(){
+        return Customer_Retrieval_Rate;
+    }
+    public void setCustomer_Retrieval_Rate(int customer_Retrieval_Rate){
+        Customer_Retrieval_Rate = customer_Retrieval_Rate;
+    }
+    public int getMax_Ticket_Capacity(){
+        return Max_Ticket_Capacity;
+    }
+    public void setMax_Ticket_Capacity(int max_Ticket_Capacity){
+        Max_Ticket_Capacity = max_Ticket_Capacity;
+    }
+
+
+    @Override
+    public String toString(){
+        return "System-Configured Successfully\n"+
+                "Total_tickets = "+Total_Tickets+
+                "\n Ticket release Rate = "+Ticket_Release_Rate+
+                "\n customer Retrieval Rate = "+Customer_Retrieval_Rate+
+                "\n max Ticket Capacity = "+Max_Ticket_Capacity;
+
+    }
+
+}
