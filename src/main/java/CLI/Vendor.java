8e5ee9357199feab2381d6300c83140ec9f6be75
Lakshan Fernando <162671765+lakshanFernando2003@users.noreply.github.com>
2024-12-03 18:13:20 +0530

    updated vendor class and tested it and completed

--- src/main/java/CLI/Vendor.java
+++ src/main/java/CLI/Vendor.java
@@ -1,45 +1,51 @@
 package CLI;
 
 public class Vendor implements Runnable {
-    private final Ticket_pool ticketPool; // reference Ticket pool from its class
+    private final Ticket_pool ticketPool;
     private final int ticketReleaseRate;
-    private final String VendorName;// vendor name
-    private int ticketCounter;  // tacking the number  of releasing tickets by vendor
+    private final String vendorName;
+    private int ticketCounter; // has a record of ticket issued
 
-
-
-    //constructor
-    public Vendor(Ticket_pool ticketPool, int ticketReleaseRate ,String VendorName){
+    // constructor
+    public Vendor(Ticket_pool  ticketPool, int ticketReleaseRate, String vendorName){
         this.ticketPool = ticketPool;
-        this.ticketReleaseRate = ticketReleaseRate;
-        this.VendorName =VendorName;
-        this.ticketCounter = 0; // set counter to 0
+        this.ticketReleaseRate = ticketReleaseRate*1000;
+        this.vendorName = vendorName;
+        this.ticketCounter = 0;
     }
 
     @Override
     public void run(){
         try {
-            while(ticketPool.getTotalTicketsAdded() < ticketPool.getTotalTicketLimit()){
-                // creating tickets
-                String ticket = generateTicket();
-                // Add the ticket to the pool
-                ticketPool.AddTickets(ticket);
-                //info of  adding tickets
-                System.out.println(VendorName+" added ticket : "+ ticket);
-                // wait time in ms
+            while (!ticketPool.AllTicketsSold()){
+                boolean Notify = false; // purpose is to notify only when tickets are adding
+                synchronized (ticketPool){
+                    while (ticketPool.getCurrentPoolSize() >= ticketPool.getMaxCapacity()){
+                        if (!ticketPool.WaitLog()){
+                            System.out.println("Ticket pool is full. Vendor is waiting....");
+                            ticketPool.setWaitLog(true);
+                        }
+                        ticketPool.wait();
+                    }
+                    ticketPool.setWaitLog(false);
+                    if (ticketPool.getTotalTicketsAdded() < ticketPool.getTotalTicketLimit()){
+                        String ticket = "Ticket-"+(++ticketCounter);
+                        System.out.println("⸺⸺⸺⸺⸺⸺⸺⸺⸺⸺⸺⸺⸺⸺⸺⸺⸺");
+                        System.out.println("Vendor running....");
+                        ticketPool.AddTickets(ticket);
+                        System.out.println(vendorName+" added - "+ticket+" | Tickets added "+ticketPool.getTotalTicketsAdded());
+                        Notify = true;
+                    }
+                    if (Notify) { // notify the waiting threads if notify boolean is true
+                        ticketPool.notify(); // this will notify to say a ticket is added to the pool
+                    }
+                }
                 Thread.sleep(ticketReleaseRate);
             }
-            System.out.println(VendorName + " has reached the total ticket limit. Stopping production.");
         }catch (InterruptedException e){
-            System.out.println(VendorName +"has been interrupted. Stopped creating Tickets .");
-            Thread.currentThread().interrupt(); // restore the interrupted status
+            System.out.println(vendorName + " was interrupted.");
+            Thread.currentThread().interrupt();
         }
-    }
 
-    // Generates a unique ticket string
-    private String generateTicket() {
-        ticketCounter++;
-        return VendorName + "-TICKET-" + ticketCounter;
     }
-
 }
